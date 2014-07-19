057aad95d7431d4c614cff172e66858f32406479
Aaron Lewis Cunnington <aaron@intellisearch.no>
2014-07-19 11:36:53 +0100

    Split commands from main application. Added a way to stringify native DOM elements! Now we can return all this information to the client so it has a lot more information about what the element contains and what actions it can support.

--- main.js
+++ main.js
@@ -1,289 +1,129 @@
 var db = require('mongodb');
 var events = require('events');
 var fs = require('fs');
-var webdriver = require('selenium-webdriver');
+var commands = require('./mate-commands').commands;
 
 var args = process.argv;
 if(args.length < 3) { process.exit(); }
 
-var driver = new webdriver.Builder()
-	.withCapabilities(webdriver.Capabilities.phantomjs())
-	.build();
-
-var campaign = {
-	'id': args[2],
-	'complete': false,
-	'timer': 0,
-	'limit': 5
-};
-
-var helperFunctions = {
-	'stringifyDOM': function(orig, tmp, level) {
-	    var tmp = tmp || {};
-	    var level = level || 0;
-	    for(var i in orig) {
-	        if(!orig[i]) { continue; }
-	        if(typeof(orig[i]) === 'function' || typeof(orig[i]) === 'object' || level > 1) { continue; }
-	        if(typeof(orig[i]) === 'array') { level++; helperFunctions.stringifyDOM(orig[i], tmp, level); continue; }
-	        tmp[i] = orig[i]; continue;
-	    }
-	    return JSON.stringify(tmp);
-	}
-};
-
-var commands = {
-
-	'click': function(selector, callback) {
-
-		driver.findElement(webdriver.By.css(selector)).click();
-
-		callback();
-
-	},
-
-	'done': function(data, callback) {
-
-		callback({});
-
-	},
-
-	'open': function(url, callback) {
-
-		var evalGetUrl = function() {
-			return location.href;
-		};
-
-		driver.get(url);
-		driver.executeScript(evalGetUrl).then( function(actualUrl) {
-			callback({'url': actualUrl});
-		});
-
-	},
-
-	'screenshot': function(filename, callback) {
-
-		filename = filename || new Date().getTime() + Math.random().toString().replace(/\./, '0') + '.png';
-		filename = 'screenshots/' + filename;
-
-		driver.takeScreenshot().then( function(data) {
-			fs.writeFileSync(filename, data, {'encoding': 'base64'});
-			callback({'filename': filename});
-		});
-
-	},
-
-	'scrollPageToEnd': function(data, callback) {
-
-		var self = this;
-
-		this.eventEmitter = new events.EventEmitter();
-
-		var data = data || {};
-
-		var evalScroll = function() {
-			window.scrollTo(0, document.body.scrollHeight);
-			return document.body.scrollTop;
-		};
-
-		this.eventEmitter.on('scroll', function() {
-
-			data.prevScrollTop = data.prevScrollTop || 0;
-			data.tries = data.tries || 0;
-			data.tryLimit = data.tryLimit || 5;
-
-			driver.executeScript(evalScroll).then( function(scrollTop) {
-				self.eventEmitter.emit('processScroll', scrollTop);
-			});
-
-		});
-
-		this.eventEmitter.on('processScroll', function(scrollTop) {
-
-			console.log('Scrolled to ' + scrollTop);
-
-			data.scrollTop = scrollTop;
-
-			if(data.scrollTop == data.prevScrollTop) {
-				data.tries++;
-			}
-			else {
-				data.tries = 0;
-			}
-
-			data.prevScrollTop = scrollTop;
-
-			if(data.tries >= data.tryLimit) {
-				self.eventEmitter.emit('done', scrollTop);
-				return;
-			}
-			else {
-				setTimeout( function() {
-					self.eventEmitter.emit('scroll');
-				}, 1000);
-			}
-
-		});
-
-		this.eventEmitter.on('done', function(scrollTop) {
-
-			data.prevScrollTop = scrollTop;
-			if(callback) { callback({'scrollTop': scrollTop}); }
-			return;
-
-		});
-
-		this.eventEmitter.emit('scroll');
-		return;
-
-	},
-
-	'select': function(selector, callback) {
-
-		driver.findElement(webdriver.By.css(selector)).then( function(el) {
-			callback({
-				'data': {
-					'count': 1
-				},
-				'native': el
-			});
-		});
-
-	},
-
-	'selectAll': function(selector, callback) {
-
-		driver.findElements(webdriver.By.css(selector)).then( function(els) {
-			callback({
-				'data': {
-					'count': els.length
-				},
-				'native': els
-			});
-		});
-
-	},
-
-	'sendKeys': function(data, callback) {
-
-		var self = this;
-
-		this.eventEmitter = new events.EventEmitter();
-
-		var selector = data.selector;
-		var string = data.string;
-
-		driver.findElement(webdriver.By.css(selector)).sendKeys(string);
-
-		callback();
-
-	}
-
-};
-
 var Mate = function() {
-
-	this.fileName = '';
-	this.data = [];
-	this.step = 0;
-
-	this.eventEmitter = new events.EventEmitter();
 
 	var self = this;
 
-	this.eventEmitter.on('save', function() {
+	this.campaign = {
+		id: null,
+		complete: false,
+		timer: 0,
+		limit: 5
+	};
+	this.fileName = '';
+	this.data = [];
+	this.step = 0;
+	this.eventEmitter = new events.EventEmitter();
 
-		fs.writeFileSync(self.fileName, JSON.stringify(self.data, null, '\t'), {'encoding': 'utf-8'});
+	this.setCampaign = function(campaign) {
 
-	});
+		self.campaign.id = campaign;
 
-	this.eventEmitter.on('load', function() {
+	};
 
-		self.fileName = campaign.id + '.json';
-		var content = fs.readFileSync(self.fileName, {'encoding': 'utf-8'});
-		self.data = JSON.parse(content);
-		self.step = 0;
-		self.eventEmitter.emit('processCommand');
+	this.exec = function() {
 
-	});
+		self.eventEmitter.on('save', function() {
 
-	this.eventEmitter.on('processCommand', function() {
+			fs.writeFileSync(self.fileName, JSON.stringify(self.data, null, '\t'), {'encoding': 'utf-8'});
 
-		self.data[self.step].processed = self.data[self.step].processed || false;
-		if(self.data[self.step].processed) {
-			self.eventEmitter.emit('processNextCommand');
-			return;
-		}
-		self.data[self.step].data = self.data[self.step].data || null;
-		self.data[self.step].waiting = true;
-		self.eventEmitter.emit('main.save');
-		console.log('Command: ', self.data[self.step].command);
-		console.log('Data:    ', self.data[self.step].data);
-		if(!commands[self.data[self.step].command]) {
-			self.eventEmitter.emit('processNextCommand', 'Command `' + self.data[self.step].command + '` does not exist');
-			return;
-		};
-		commands[self.data[self.step].command](self.data[self.step].data, function(res) {
-			self.eventEmitter.emit('commandProcessed', res);
 		});
 
-	});
+		self.eventEmitter.on('load', function() {
 
-	this.eventEmitter.on('processNextCommand', function(reason) {
+			self.fileName = self.campaign.id + '.json';
+			var content = fs.readFileSync(self.fileName, {'encoding': 'utf-8'});
+			self.data = JSON.parse(content);
+			self.step = 0;
+			self.eventEmitter.emit('processCommand');
 
-		if(reason) { console.log(reason); }
-		self.step++;
-		if(self.step >= self.data.length) { 
-			self.eventEmitter.emit('waitForCommands');
-			return;
-		}
-		self.eventEmitter.emit('processCommand');
+		});
 
-	});
+		self.eventEmitter.on('processCommand', function() {
 
-	this.eventEmitter.on('commandProcessed', function(res) {
+			self.data[self.step].processed = self.data[self.step].processed || false;
+			if(self.data[self.step].processed) {
+				self.eventEmitter.emit('processNextCommand');
+				return;
+			}
+			self.data[self.step].data = self.data[self.step].data || null;
+			self.data[self.step].waiting = true;
+			self.eventEmitter.emit('main.save');
+			console.log('Command: ', self.data[self.step].command);
+			console.log('Data:    ', self.data[self.step].data);
+			if(!commands[self.data[self.step].command]) {
+				self.eventEmitter.emit('processNextCommand', 'Command `' + self.data[self.step].command + '` does not exist');
+				return;
+			};
+			commands[self.data[self.step].command](self.data[self.step].data, function(res) {
+				self.eventEmitter.emit('commandProcessed', res);
+			});
 
-		res = res || {};
-		res.command = self.data[self.step].command;
+		});
 
-		var printable = {};
-		for(var i in res) { if(i == 'native') { continue; } printable[i] = res[i]; }
+		self.eventEmitter.on('processNextCommand', function(reason) {
 
-		console.log(JSON.stringify(printable));
-		self.data[self.step].waiting = false;
-		self.data[self.step].processed = true;
-		self.eventEmitter.emit('save');
-		if(self.data[self.step].command == 'done') {
-			self.eventEmitter.emit('done');
-			return;
-		}
-		self.eventEmitter.emit('processNextCommand');
+			if(reason) { console.log(reason); }
+			self.step++;
+			if(self.step >= self.data.length) { 
+				self.eventEmitter.emit('waitForCommands');
+				return;
+			}
+			self.eventEmitter.emit('processCommand');
 
-	});
+		});
 
-	this.eventEmitter.on('done', function() {
+		self.eventEmitter.on('commandProcessed', function(res) {
 
-		campaign.complete = true;
+			res = res || {};
+			res.command = self.data[self.step].command;
 
-	});
+			console.log(res);
+			console.log('\n---\n');
+			self.data[self.step].waiting = false;
+			self.data[self.step].processed = true;
+			self.eventEmitter.emit('save');
+			if(self.data[self.step].command == 'done') {
+				self.eventEmitter.emit('done');
+				return;
+			}
+			self.eventEmitter.emit('processNextCommand');
 
-	this.eventEmitter.on('waitForCommands', function() {
+		});
 
-		//console.log('Waiting for commands...');
-		campaign.timer++;
-		setTimeout( function() {
-			self.eventEmitter.emit('load');
-		}, 1000);
+		self.eventEmitter.on('done', function() {
 
-	});
+			self.campaign.complete = true;
 
-	this.eventEmitter.emit('load');
+		});
+
+		self.eventEmitter.on('waitForCommands', function() {
+
+			self.campaign.timer++;
+			setTimeout( function() {
+				self.eventEmitter.emit('load');
+			}, 1000);
+
+		});
+
+		self.eventEmitter.emit('load');
+
+	};
 
 };
 
 var mate = new Mate();
+mate.setCampaign(args[2]);
+mate.exec();
 
 (function wait() {
-	if(!campaign.complete && campaign.timer < campaign.limit) {
+	if(!mate.campaign.complete && mate.campaign.timer < mate.campaign.limit) {
 		setTimeout(wait, 1000);
 	}
 })();
---
+++ mate-commands.js
@@ -0,0 +1,215 @@
+var webdriver = require('selenium-webdriver');
+var driver = new webdriver.Builder()
+	.withCapabilities(webdriver.Capabilities.phantomjs())
+	.build();
+var events = require('events');
+
+var commands = {
+
+	'click': function(selector, callback) {
+
+		driver.findElement(webdriver.By.css(selector)).click();
+
+		callback();
+
+	},
+
+	'done': function(data, callback) {
+
+		callback({});
+
+	},
+
+	'open': function(url, callback) {
+
+		var evalGetUrl = function() {
+			return location.href;
+		};
+
+		driver.get(url);
+		driver.executeScript(evalGetUrl).then( function(actualUrl) {
+			callback({'url': actualUrl});
+		});
+
+	},
+
+	'screenshot': function(filename, callback) {
+
+		filename = filename || new Date().getTime() + Math.random().toString().replace(/\./, '0') + '.png';
+		filename = 'screenshots/' + filename;
+
+		driver.takeScreenshot().then( function(data) {
+			fs.writeFileSync(filename, data, {'encoding': 'base64'});
+			callback({'filename': filename});
+		});
+
+	},
+
+	'scrollPageToEnd': function(data, callback) {
+
+		var self = this;
+
+		this.eventEmitter = new events.EventEmitter();
+
+		var data = data || {};
+
+		var evalScroll = function() {
+			window.scrollTo(0, document.body.scrollHeight);
+			return document.body.scrollTop;
+		};
+
+		this.eventEmitter.on('scroll', function() {
+
+			data.prevScrollTop = data.prevScrollTop || 0;
+			data.tries = data.tries || 0;
+			data.tryLimit = data.tryLimit || 5;
+
+			driver.executeScript(evalScroll).then( function(scrollTop) {
+				self.eventEmitter.emit('processScroll', scrollTop);
+			});
+
+		});
+
+		this.eventEmitter.on('processScroll', function(scrollTop) {
+
+			console.log('Scrolled to ' + scrollTop);
+
+			data.scrollTop = scrollTop;
+
+			if(data.scrollTop == data.prevScrollTop) {
+				data.tries++;
+			}
+			else {
+				data.tries = 0;
+			}
+
+			data.prevScrollTop = scrollTop;
+
+			if(data.tries >= data.tryLimit) {
+				self.eventEmitter.emit('done', scrollTop);
+				return;
+			}
+			else {
+				setTimeout( function() {
+					self.eventEmitter.emit('scroll');
+				}, 1000);
+			}
+
+		});
+
+		this.eventEmitter.on('done', function(scrollTop) {
+
+			data.prevScrollTop = scrollTop;
+			if(callback) { callback({'scrollTop': scrollTop}); }
+			return;
+
+		});
+
+		this.eventEmitter.emit('scroll');
+		return;
+
+	},
+
+	'select': function(selector, callback) {
+
+		var evalSelect = function(selector) {
+
+			function getElementEssentials(orig, tmp, level) {
+			    var tmp = tmp || {};
+			    var level = level || 0;
+			    for(var i in orig) {
+			        if(!orig[i]) { continue; }
+			        if(!i.match(/[a-z]/)) { continue; }
+			        if(typeof(orig[i]) === 'function' || typeof(orig[i]) === 'object' || level > 1) { continue; }
+			        if(typeof(orig[i]) === 'array') { level++; get(orig[i], tmp, level); continue; }
+			        tmp[i] = orig[i];
+			        continue;
+			    }
+			    return tmp;
+			}
+
+			var el = document.querySelector(selector);
+
+			var el2 = getElementEssentials(el);
+
+			var elStringified = JSON.stringify(el2);
+
+			return elStringified;
+
+		};
+
+		driver.findElement(webdriver.By.css(selector)).then( function(el) {
+
+			driver.executeScript(evalSelect, selector).then( function(nativeEl) {
+				callback({
+					'data': nativeEl
+				});
+			});
+
+		});
+
+	},
+
+	'selectAll': function(selector, callback) {
+
+		var evalSelectAll = function(selector) {
+
+			function getElementEssentials(orig, tmp, level) {
+			    var tmp = tmp || {};
+			    var level = level || 0;
+			    for(var i in orig) {
+			        if(!orig[i]) { continue; }
+			        if(!i.match(/[a-z]/)) { continue; }
+			        if(typeof(orig[i]) === 'function' || typeof(orig[i]) === 'object' || level > 1) { continue; }
+			        if(typeof(orig[i]) === 'array') { level++; get(orig[i], tmp, level); continue; }
+			        tmp[i] = orig[i];
+			        continue;
+			    }
+			    return tmp;
+			}
+
+			var els = document.querySelectorAll(selector);
+			
+			var els2 = [];
+			for(var i in els) {
+				els2.push(getElementEssentials(els[i]));
+			}
+
+			//var els2 = {test1: true, test2: [1, 2, 3], test3: [false]};
+
+			var elsStringified = JSON.stringify(els2);
+
+			return elsStringified;
+
+		};
+
+		driver.findElements(webdriver.By.css(selector)).then( function(els) {
+
+			driver.executeScript(evalSelectAll, selector).then( function(nativeEls) {
+				callback({
+					'data': nativeEls
+				});
+			});
+
+		});
+
+	},
+
+	'sendKeys': function(data, callback) {
+
+		var self = this;
+
+		this.eventEmitter = new events.EventEmitter();
+
+		var selector = data.selector;
+		var string = data.string;
+
+		driver.findElement(webdriver.By.css(selector)).sendKeys(string);
+
+		callback();
+
+	}
+
+};
+
+exports.commands = commands;
